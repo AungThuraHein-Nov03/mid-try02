73b481aa6023883e72134c43c909ed15e859c893
AungThuraHein <u6726135@au.edu>
2026-01-06 17:05:24 +0700

    added ref,state and fixed positions

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,8 +1,17 @@
+import {useRef} from 'react'
+
 function Home() { 
+    const inputRef = useRef()
+    function handleClick() { 
+        inputRef.current.focus()
+    }
     return(
-        <div>
-            <p> TEST TEST TEST </p>
+        <div className='body'> 
+            <input type = "text" ref = {inputRef} placeholder="Type something ..."/>
+            <br/><br/>
+            <button onClick = {handleClick}>Focus Input </button>
         </div>
+
 
     )
 }
--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -1,9 +1,17 @@
+import {useState} from 'react'
+
 function Profile() { 
+    const[Name, setName] = useState("");
+    function onNameChange(event) {
+            setName(event.target.value);
+        }
     return(
-        <div>
-            <p> I'm Austin.</p>
-        </div>
+       <div className = "top-left">
+        <input type = "text" id = "Name" value = {Name} onChange={onNameChange}/>
+        <p> Your name is {Name}</p> 
+       </div>
     )
 }
+
 
 export default Profile
